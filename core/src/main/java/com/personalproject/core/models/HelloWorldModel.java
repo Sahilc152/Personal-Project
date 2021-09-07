ada6323da344136c89a37bf9851f41d61e946b9f
Sahilc152 <sahil.chhabria@ugamsolutions.com>
2021-09-07 14:37:31 +0530

    Hello World Java updated

--- core/src/main/java/com/personalproject/core/models/HelloWorldModel.java
+++ core/src/main/java/com/personalproject/core/models/HelloWorldModel.java
@@ -58,7 +58,7 @@ public class HelloWorldModel {
                 .map(Page::getPath).orElse("");
 
         message = "Hello World!\n"
-            + "Resource type is: " + resourceType + "\n"
+            + "Resource type : " + resourceType + "\n"
             + "Current page is:  " + currentPagePath + "\n"
             + "This is instance: " + settings.getSlingId() + "\n";
     }
